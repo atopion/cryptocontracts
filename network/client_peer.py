54b602ab27d7660a820efeb92bd120d35d5939b8
relawe <50918981+relawe@users.noreply.github.com>
2019-06-11 17:25:33 +0200

    Add files via upload
    Server started by creating a server object, client by creating object and exec the function connectToNet()

---
+++ network/client_peer.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  5 23:32:58 2019
+
+@author: rene
+"""
+
+import socket
+import threading
+
+
+class Peer:
+    """ A class representing a Peer of the network which connects to the server
+    
+    Attributes
+    ----------
+    activePeers : list
+        a list containing all peers that are currently connected to the network
+    serverAddress : str
+        a string representing the IP address of the server peer
+    sock : socket object
+        a socket that is bind to handle the connection
+        
+    Methods
+    -------
+    commandHandler()
+        Takes care of the user input
+    connectToNet()
+        Establishes connection to the server peer
+    disconnectFromNet()
+        Disconnects from the network/server peer
+    getActivePeers()
+        Gets the list of all peers currently connected to the net/server peer
+    requestGraph(address)
+        Sends request for latest version of graph to peer specified in address
+    sendGraph(address)
+        Sends latest version of graph to peer specified in address
+    """
+    
+    activePeers = []
+    serverAddress = "127.0.0.1"
+    sock = None
+    
+    def __init__(self):
+        pass
+    
+    
+    def commandHandler(self):
+        """ Takes care of the user input. 
+        
+        The command 'peer' shows the other active peers in the network.
+        the commands 'exit', 'quit' and 'close' end the connection.
+        """
+        while True:
+            i = input()
+            if i == "peers":
+                p = self.getActivePeers()
+                print(p)
+            else:
+                if i == "exit" or i == "quit" or i == "close":
+                    self.disconnectFromNet()
+#                distinguish between peers and conenctions
+#            else:
+#                for connection in self.connections:
+#                    connection.send(bytes(str(i),'utf-8'))
+    
+    def connectToNet(self):
+        """Establishes connection to the server peer
+        
+        Sets up a socket and connects to the server peer. A new thread is started
+        that runs the commandHandler function for user input handling.
+        Data that is received is checked for special meaning by prefixes.
+        If no prefix is found, it is considered a message and printed out.
+        """
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        self.sock.connect((self.serverAddress,10000))
+        
+        print("connected to server")
+        
+        commandThread = threading.Thread(target=self.commandHandler)
+        commandThread.daemon = True
+        commandThread.start()
+        
+        while True:
+            data = self.sock.recv(1024)
+            if not data:
+                break
+            
+            # look for specific prefix indicating the list of active peers
+            if data[0:1] == b'\x11':
+                self.activePeers = str(data[1:], "utf-8").split(",")[:-1]
+                
+            # if no prefix consider data a message and print it
+            else:
+                print(str(data,'utf-8'))
+        
+
+    def disconnectFromNet(self):
+        """Disconnects from the network/server peer
+        
+        The socket is shutdown and closed which causes a disconnection
+        """
+        self.sock.shutdown(socket.SHUT_RDWR)
+        self.sock.close()
+    
+    def getActivePeers(self):
+        """ returns the list of all peers currently connected to the net/server peer
+        
+        The peer IP addresses are taken from the object variable activePeer and are joined in a String
+        
+        Returns
+        -------
+        string
+            a string representing the IP addresses of the currently connected peers
+        """
+        
+        p = ""
+        for peer in self.activePeers:
+            p = p + peer + ", "
+        return p
+    
+    def requestGraph(self, address):
+        """Sends request for latest version of graph to peer specified in address
+        
+        Parameters
+        ----------
+        address : str
+            IP address of peer to send request to
+            
+        """
+        
+        pass
+    
+    def sendGraph(self, address):
+        """Sends latest version of graph to peer specified in address
+        
+        Parameters
+        ----------
+        address : str
+            IP address of peer to send graph to
+        """
+        
+        pass
+    
+    
+#client = Peer() 
+#client.connectToNet()  
---
+++ network/server_peer.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  5 23:47:34 2019
+
+@author: rene
+"""
+
+import socket
+import threading
+
+
+class ServerPeer:
+    """ A class representing the server peer of the network which the client peers can connect to
+    
+    Attributes
+    ----------
+    activePeers : list
+        a list containing all peers that are currently connected to the network
+    connections : list
+        a list containing the connections currently established with the clients
+        
+    Methods
+    -------
+    commandHandler()
+        Takes care of the user input
+    connectionHandler()
+        Manages connections to client peers
+    getActivePeers()
+        Gets the list of all peers currently connected to the net/server peer
+    requestGraph(address)
+        Sends request for latest version of graph to peer specified in address
+    sendActivePeers()
+        Sends the list of active peers to all clients connected to this server
+    sendGraph(address)
+        Sends latest version of graph to peer specified in address
+    """    
+    
+    connections = []
+    activePeers = []
+    
+    def __init__(self):
+        """ Constructor that initiats general server functions
+        
+        A socket object is created, bind and starts listening to connections.
+        The commandhandler is started in a dedicated thread.
+        Incoming connections are handeled by one thread each.
+        When a connection is established the address and connection are stored in the activePeers and connections list.
+        Moreover, the list of all active peers is send.
+        
+        """
+        
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        sock.bind(('0.0.0.0',10000))
+        sock.listen(1)
+        
+        hostname = socket.gethostname()
+        hostaddr = socket.gethostbyname(hostname)
+        self.activePeers.append(hostaddr)
+        
+        print("Server running...")
+        
+        commandThread = threading.Thread(target=self.commandHandler)
+        commandThread.daemon = True
+        commandThread.start()
+        
+        while True:
+            conn,addr = sock.accept()
+            thread = threading.Thread(target=self.connectionHandler, args=(conn,addr))
+            thread.daemon = True
+            thread.start()
+            self.connections.append(conn)
+            self.activePeers.append(addr[0])
+            print(str(addr[0]) + ':' + str(addr[1]),"connected")
+            self.sendActivePeers()
+            
+        
+    def commandHandler(self):
+        """ Takes care of the user input. 
+        
+        The command 'peers' shows the other active peers in the network.
+        """
+        
+        while True:
+            i = input()
+            if i == "peers":
+                self.getActivePeers()
+            else:
+                for connection in self.connections:
+                    connection.send(bytes(str(i),'utf-8'))
+                    
+    
+    def connectionHandler(self, conn, addr):
+        """ Manages connections to client peers
+        
+        It is constantly waited for incoming data. The data is a byte stream.
+        The data sent by a client is distributed to all the other ones.
+        After a connection has been canceled, the new connection list is send to all the active peers in the network.
+        
+        parameters:
+        -----------
+        conn : connection of specific peer
+        addr : IP address of specific peer
+        """
+        
+        while True:
+            data = conn.recv(1024)
+            for connection in self.connections:
+                connection.send(bytes(data))    
+            if not data:
+                print(str(addr[0]) + ":" + str(addr[1]),"disconnected")
+                self.connections.remove(conn)
+                self.activePeers.remove(addr[0])
+                conn.close()
+                self.sendActivePeers()
+                break
+    
+#    def connectToNet(self):
+#        pass
+#    
+#    def disconnectFromNet(self):
+#        pass
+    
+    def getActivePeers(self):
+        """ returns the list of all peers currently connected to this net/server peer
+        
+        The peer IP addresses are taken from the object variable activePeer and are joined in a String
+        
+        Returns
+        -------
+        string
+            a string representing the IP addresses of the currently connected peers
+        """
+        
+        p = ""
+        for peer in self.activePeers:
+            p = p + peer + ", "
+        return p
+    
+    
+    
+    def requestGraph(self, address):
+        """ Sends request for latest version of graph to peer specified in address
+        
+        Parameters
+        ----------
+        address : str
+            IP address of peer to send request to
+            
+        """
+        
+        pass
+    
+    def sendActivePeers(self):
+        """ Sends the list of active peers to all the clients connected to this server.
+        
+        The list of all active peers is taken and send over every connection as a byte stream.
+        A dedicated information is output to the user.
+        """
+        
+        p = self.getActivePeers()
+
+        for connection in self.connections:
+            connection.send(b'\x11' + bytes(p, "utf-8"))
+        print("peer list sent!")
+    
+    
+    def sendGraph(self, address):
+        """Sends latest version of graph to peer specified in address
+        
+        Parameters
+        ----------
+        address : str
+            IP address of peer to send graph to
+        """
+        
+        pass
+    
+    
+#server = ServerPeer()
+    
