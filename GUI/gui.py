7bbeb2f6b54742d29e3a985488f97a74e316b78e
THaar50 <50915555+THaar50@users.noreply.github.com>
2019-07-03 15:56:06 +0200

    Layout fixes, progressbar bugs fixed, resolution bugs fixed

--- GUI/gui.py
+++ GUI/gui.py
@@ -2,15 +2,15 @@ import sys
 import time
 import os
 from PyQt5.QtWidgets import (QMainWindow, QAction, qApp, QApplication, QPushButton, QProgressBar, QInputDialog,
-                             QLineEdit, QFileDialog as Dialog)
+                             QLineEdit, QVBoxLayout, QHBoxLayout, QSpacerItem, QSizePolicy, QWidget, QFileDialog as Dialog)
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QUrl
-#from core import core
+from Core import core
 
 
 class GUI(QMainWindow):
 
-    def __init__(self):
+    def __init__(self, width, height):
         super().__init__()
         self.progress = QProgressBar(self)
         self.file_label = QLineEdit(self)
@@ -20,94 +20,131 @@ class GUI(QMainWindow):
         self.PROGRESS_MAX = 100
         self.DEFAULT_STRING = "No Data"
         self.statusBar()
-
+        self.width = width
+        self.height = height
         self.init_ui()
 
     def init_ui(self):
 
-        self.setGeometry(300, 300, 900, 640)
-        self.setMinimumSize(900, 640)
+        self.setGeometry(0, 0, self.width/2, self.height/2)
+        self.setMinimumSize(self.width/3, self.height/3)
         self.setWindowTitle('CryptoContracts')
 
         exit_action = QAction(QIcon('exit.png'), '&Exit', self)
         exit_action.setShortcut('Ctrl+Q')
-        # exit_action.setStatusTip('Exit application')
         exit_action.triggered.connect(qApp.quit)
 
         select_button = QPushButton('Select PDF...', self)
-        select_button.move(310, 30)
         select_button.clicked.connect(self.clicked_select_file)
 
         sign1_button = QPushButton('Signature1...', self)
-        sign1_button.move(310, 70)
         sign1_button.clicked.connect(self.clicked_signature1)
 
         sign2_button = QPushButton('Signature2...', self)
-        sign2_button.move(310, 110)
         sign2_button.clicked.connect(self.clicked_signature2)
 
-        connect_button = QPushButton('Connect...', self)
-        connect_button.move(310, 150)
+        connect_button = QPushButton('Connect peer...', self)
         connect_button.clicked.connect(self.clicked_connect)
 
         add_to_layer_button = QPushButton('Add to chain...', self)
-        add_to_layer_button.move(405, 240)
         add_to_layer_button.clicked.connect(self.clicked_add_to_layer)
 
-        self.file_label.move(420, 33)
-        self.file_label.resize(180, 25)
         self.file_label.setText(self.DEFAULT_STRING)
+        self.file_label.setReadOnly(True)
 
-        self.sign1_label.move(420, 73)
-        self.sign1_label.resize(180, 25)
         self.sign1_label.setText(self.DEFAULT_STRING)
+        self.sign1_label.setReadOnly(True)
 
-        self.sign2_label.move(420, 113)
-        self.sign2_label.resize(180, 25)
         self.sign2_label.setText(self.DEFAULT_STRING)
+        self.sign2_label.setReadOnly(True)
 
-        self.conn_label.move(420, 153)
-        self.conn_label.resize(180, 25)
         self.conn_label.setText(self.DEFAULT_STRING)
+        self.conn_label.setReadOnly(True)
 
-        self.progress.setGeometry(310, 200, 325, 25)
         self.progress.setMaximum(self.PROGRESS_MAX)
+        self.progress.setValue(0)
+        self.progress.setTextVisible(False)
 
         menubar = self.menuBar()
         file_menu = menubar.addMenu('&File')
         file_menu.addAction(exit_action)
 
+        widget = QWidget()
+
+        vbox1 = QVBoxLayout()
+        vbox1.addWidget(select_button)
+        vbox1.addWidget(sign1_button)
+        vbox1.addWidget(sign2_button)
+        vbox1.addWidget(connect_button)
+
+        vbox2 = QVBoxLayout()
+        vbox2.addWidget(self.file_label)
+        vbox2.addSpacing(3)
+        vbox2.addWidget(self.sign1_label)
+        vbox2.addSpacing(3)
+        vbox2.addWidget(self.sign2_label)
+        vbox2.addSpacing(3)
+        vbox2.addWidget(self.conn_label)
+
+        hbox = QHBoxLayout()
+        hbox.addLayout(vbox1)
+        hbox.addLayout(vbox2)
+
+        vbox3 = QVBoxLayout()
+        vbox3.addWidget(add_to_layer_button)
+        vbox3.addWidget(self.progress)
+
+        vbox4 = QVBoxLayout()
+        vbox4.addStretch(1)
+        vbox4.addLayout(hbox)
+        vbox4.addSpacing(10)
+        vbox4.addLayout(vbox3)
+        vbox4.addStretch(10)
+
+        spaceItem = QSpacerItem(self.width/5, self.height/5, QSizePolicy.Expanding)
+        hbox2 = QHBoxLayout()
+        hbox2.addSpacerItem(spaceItem)
+        hbox2.addLayout(vbox4)
+        hbox2.addSpacerItem(spaceItem)
+
+        widget.setLayout(hbox2)
+        self.setCentralWidget(widget)
+
         self.show()
+
+    def update_progress(self, line_edit, s):
+        if not s:
+            if not line_edit.text() == self.DEFAULT_STRING:
+                line_edit.setText(self.DEFAULT_STRING)
+                self.progress.setValue(self.progress.value() - self.PROGRESS_MAX / 5)
+            print("Canceled")
+        elif line_edit.text() == self.DEFAULT_STRING:
+            if not line_edit.text() == s:
+                self.progress.setValue(self.progress.value() + self.PROGRESS_MAX / 5)
+            line_edit.setText(s)
+        else:
+            line_edit.setText(s)
 
     def clicked_select_file(self):
         file = Dialog.getOpenFileName(filter='*.pdf')[0]
-        if self.file_label.text() == self.DEFAULT_STRING and file != "":
-            if not self.file_label.text() == file:
-                self.progress.setValue(self.progress.value() + self.PROGRESS_MAX / 5)
-        self.file_label.setText(file)
+        self.update_progress(self.file_label, file)
         # checksum calling crashes program
         # return file_hash = core.checksum()
         print(file)
 
     def clicked_signature1(self):
         sig = Dialog.getOpenFileName(filter='*.pdf')[0]
-        if self.sign1_label.text() == self.DEFAULT_STRING and sig != "":
-            if not self.sign1_label.text() == sig:
-                self.progress.setValue(self.progress.value() + self.PROGRESS_MAX / 5)
-        self.sign1_label.setText(sig)
+        self.update_progress(self.sign1_label, sig)
 
-        # url = QUrl.fromLocalFile(sig)
-        # print(os.path.splitext(url.fileName())[0])
+        url = QUrl.fromLocalFile(sig)
+        print(os.path.splitext(url.fileName())[0])
         # checksum calling crashes program
-        # sig_hash = core.checksum(url.fileName())
-        print(sig)
+        sig_hash = core.checksum(path=sig)
+        print(sig_hash)
 
     def clicked_signature2(self):
         sig = Dialog.getOpenFileName(filter='*.pdf')[0]
-        if self.sign2_label.text() == self.DEFAULT_STRING and sig != "":
-            if not self.sign2_label.text() == sig:
-                self.progress.setValue(self.progress.value() + self.PROGRESS_MAX / 5)
-        self.sign2_label.setText(sig)
+        self.update_progress(self.sign2_label, sig)
         # checksum calling crashes program
         # sig_hash = core.checksum(sig)
         print(sig)
@@ -116,13 +153,15 @@ class GUI(QMainWindow):
         # establish connection with other client
         ip, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter ip address of other client:')
         if ok and self.validate_ip(ip):
-            if self.conn_label.text() == self.DEFAULT_STRING:
-                if not self.conn_label.text() == ip:
-                    self.progress.setValue(self.progress.value() + self.PROGRESS_MAX / 5)
-            self.conn_label.setText(ip)
+            self.update_progress(self.conn_label, ip)
         else:
-            print("IP wrong")
+            ip = ""
+            self.update_progress(self.conn_label, ip)
         print(ip)
+
+    def clicked_add_to_layer(self):
+        # broadcast transmission to all peers if block verified
+        print("Nothing yet")
 
     def validate_ip(self, s):
         a = s.split('.')
@@ -136,15 +175,6 @@ class GUI(QMainWindow):
                 return False
         return True
 
-    def clicked_add_to_layer(self):
-        # broadcast transmission to all peers
-        print("Nothing yet")
-
-    # def update_progress(self, s):
-    #    if s == self.DEFAULT_STRING:
-    #        if not s == sig:
-    #            self.progress.setValue(self.progress.value() + self.PROGRESS_MAX / 5)
-
 
 stylesheet = """
     GUI {
@@ -156,5 +186,6 @@ stylesheet = """
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     app.setStyleSheet(stylesheet)
-    ex = GUI()
+    screen = app.primaryScreen()
+    ex = GUI(screen.size().width(), screen.size().height())
     sys.exit(app.exec_())
