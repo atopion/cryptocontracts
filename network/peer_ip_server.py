b10fe51415d77ae7764c16ed4b00b7541aab4a51
Rene Wetzelt <renelars.wetzelt@stud.uni-goettingen.de>
2019-07-27 11:35:51 +0200

    continued commenting

--- network/peer_ip_server.py
+++ network/peer_ip_server.py
@@ -82,6 +82,7 @@ class Peer:
     active_connectable_addresses = []   # addresses of active peers in the net to connect to
     address_connection_pairs = {}   # mapping of addresses from incoming connection and outgoing connection of same peer
     client_sockets = []     # sockets over that this peer connects to other peers
+    BUFFERSIZE = 4096   # buffersize for data length when receiving over sockets
     connected_peers = []    # Addresses of peers connected to this host
     connections = []    # holds connection objects of the server socket
     gui_socket = None   # socket for connection to the GUI 
@@ -454,6 +455,20 @@ class Peer:
             
         return p
     
+    def get_host_addr(self):
+        """ Returns the IP address of this host.
+
+        Returns
+        -------
+        tuple (str,int)
+            a tuple containing the IP address of this host and the port that the server socket is bound to
+        """
+
+        host_name = socket.gethostname()
+        host_addr = socket.gethostbyname(host_name)
+
+        return (host_addr, self.port)
+    
     def get_peers_from_ip_server(self):
         """ Gets the addresses of the active nodes in the network from the IP Server.
         
@@ -534,7 +549,7 @@ class Peer:
             while True:
                 try:
                     # receive commands from GUI
-                    data = gui_conn.recv(4096)
+                    data = gui_conn.recv(self.BUFFERSIZE)
                     if not data:
                         if self.output == "debug":
                             print("{}: GUI closed".format(self.get_time()))
@@ -592,7 +607,7 @@ class Peer:
         while True:
             try:
                 # wait for receiving data
-                data = conn.recv(4096)
+                data = conn.recv(self.BUFFERSIZE)
 
                 # peer disconnected
                 if not data:
@@ -681,7 +696,7 @@ class Peer:
                                 all_data = rec_data[1]
                                 while len(all_data) < length:   # if message longer than buffersize keep receiving until message complete
                                     to_read = length - len(all_data)
-                                    all_data += str(conn.recv(4096 if to_read > 4096 else to_read), "utf-8")
+                                    all_data += str(conn.recv(self.BUFFERSIZE if to_read > self.BUFFERSIZE else to_read), "utf-8")
 
                                 # Subchain answer
                                 if mode == 34:
@@ -782,7 +797,7 @@ class Peer:
         while True:
             try:
                 # wait for incoming data
-                data = sock.recv(4096)
+                data = sock.recv(self.BUFFERSIZE)
                 
                 # connection closed
                 if not data:
@@ -874,7 +889,7 @@ class Peer:
 #                                all_data = rec_data[1]
 #                                while len(all_data) < length:
 #                                    to_read = length - len(all_data)
-#                                    all_data += str(sock.recv(4096 if to_read > 4096 else to_read), "utf-8")
+#                                    all_data += str(sock.recv(self.BUFFERSIZE if to_read > self.BUFFERSIZE else to_read), "utf-8")
 #
 #                                if mode == 34:
 #                                    # Subchain answer
@@ -918,32 +933,42 @@ class Peer:
                     if self.output == "debug":
                         print("{}: Could not remove address from address connection pair dict".format(self.get_time()))
 
-
-######################################### continue here commenting #######################################################################
-
     def refresh_connections(self):
-        """ Gets the current active nodes in the network in a specific frequency"""
+        """ Pulls the current active peers in a specific period and connects to them
+        
+        The addresses from the active peers in the network are pulled from the IP Server.
+        The frequency of requesting is defined in REF_FREQ.
+        If a peer belonging to the newly received address is not the own host address
+        and there is not yet a connection established to, it is tried to connect to it.
+        """
         
         host_net = self.host_addr[0].split(".")  # local network address of host
         
         while True:
-            time.sleep(300)
+            REF_FREQ = 300  # refreshing-frequency in seconds
+            time.sleep(REF_FREQ)
             if self.output == "debug":
                 print("{}: Refreshing connections...".format(self.get_time()))
             self.active_connectable_addresses = self.get_peers_from_ip_server()
             
             if self.output == "debug":
                 print("{}: Active nodes: {}".format(self.get_time(),self.active_connectable_addresses))
+                
             for on in self.active_connectable_addresses:
+                # check if address in same network and if it is its own address
                 own = False
                 same_net = True
                 if on[0] == self.host_addr[0] and int(on[1]) == int(self.host_addr[1]):
                     own = True  # own address of host
                 else:
+                    # if using in same LAN or on same computer make sure to just connect to addresses in that scope (only for testing)
                     if self.scope == "internal" or self.scope == "localhost":
                         peer_net = on[0].split(".")
                         if not (peer_net[0] == host_net[0]):   # not in same LAN
                             same_net = False
+                            
+                # same_net must be true in any case to establish the connection  
+                # check if already connected to that address
                 if (not own) and same_net:
                     existing = False
                     for peer in self.server_peers:
@@ -953,53 +978,39 @@ class Peer:
                     if not existing:    # only connect to new peers
                         self.create_new_connection(on)
 
-    def get_host_addr(self):
-        """ Returns the IP address of this host.
+#    def send_offline_peer(self, address):
+#       """ Sends notification to all connected peers that another peer went offline
+#       Sends the address of its client socket over that it was connected to this host
+#
+#       Parameters
+#       ----------
+#       address : (str,int)
+#           tuple of IP address and port number of peer that went offline
+#       """
+#
+#       p = address[0] + ":" + str(address[1])
+#       for peer in self.connections:
+#            peer.send(b'\x13' + bytes(p, "utf-8") + b'!')
+#       core.network_log("SEND \\x13 to ", [x for x in self.connections])
+#       if self.output == "debug":
+#           print("Notified connected peers that {} is offline".format(p))
 
-        Returns
-        -------
-        tuple (str,int)
-            a tuple containing the IP address of this host and the port that the server socket is bound to
-        """
-
-        host_name = socket.gethostname()
-        host_addr = socket.gethostbyname(host_name)
-
-        return (host_addr, self.port)
-    
-    def send_offline_peer(self, address):
-       """ Sends notification to all connected peers that another peer went offline
-       Sends the address of its client socket over that it was connected to this host
-
-       Parameters
-       ----------
-       address : (str,int)
-           tuple of IP address and port number of peer that went offline
-       """
-
-       p = address[0] + ":" + str(address[1])
-       for peer in self.connections:
-            peer.send(b'\x13' + bytes(p, "utf-8") + b'!')
-       core.network_log("SEND \\x13 to ", [x for x in self.connections])
-       if self.output == "debug":
-           print("Notified connected peers that {} is offline".format(p))
-
-    def send_offline_connectable_address(self, address):
-        """ Sends notification to all connected peers that another peer went offline.
-        Sends the address of its connectable address
-
-        Parameters
-        ----------
-        address : (str,int)
-        tuple of IP address and port number of peer that went offline
-        """
-
-        p = address[0] + ":" + str(address[1])
-        for peer in self.connections:
-            peer.send(b'\x14' + bytes(p, "utf-8") + b'!')
-        core.network_log("SEND \\x14 to ", [x for x in self.connections])
-        if self.output == "debug":
-            print("Notified connected peers that {} is offline".format(p))
+#    def send_offline_connectable_address(self, address):
+#        """ Sends notification to all connected peers that another peer went offline.
+#        Sends the address of its connectable address
+#
+#        Parameters
+#        ----------
+#        address : (str,int)
+#        tuple of IP address and port number of peer that went offline
+#        """
+#
+#        p = address[0] + ":" + str(address[1])
+#        for peer in self.connections:
+#            peer.send(b'\x14' + bytes(p, "utf-8") + b'!')
+#        core.network_log("SEND \\x14 to ", [x for x in self.connections])
+#        if self.output == "debug":
+#            print("Notified connected peers that {} is offline".format(p))
 
     def send_port(self, address, socket=None):
         """ Sends address with port number for establishing connection to this host
@@ -1014,10 +1025,10 @@ class Peer:
 
         if socket:
             socket.send(b'\x15' + bytes(p, "utf-8") + b'!')   # ! signals end of message
-            core.network_log("SEND \\x15 to ", socket)
+            core.network_log("SEND \\x15 to ", socket)  # log statement
         else:
             address.send(b'\x15' + bytes(p, "utf-8") + b'!')
-            core.network_log("SEND \\x13 to ", address)
+            core.network_log("SEND \\x13 to ", address) # log statement
         
         if self.output == "debug":
             print("{}: Own address sent to {}:{}".format(self.get_time(), address[0], str(address[1])))
@@ -1025,20 +1036,27 @@ class Peer:
     def set_host_addr(self):
         """ Sets the IP address and the port of this host """
     
+        # if using on same computer use random loopback address
+        # because IP server stores as key value pairs and overwrites if same address (only for testing)
         if self.scope == "localhost":
             num1 = random.randint(0,199)
             num2 = random.randint(0,199)
             num3 = random.randint(0,199)
             host_addr = "127." + str(num1) + "." + str(num2) + "." + str(num3)
-            
+        
+        # if using in same LAN use local IP
         elif self.scope == "internal":
             host_name = socket.gethostname()
             host_addr = socket.gethostbyname(host_name)
 
         else:
+            # get loating IP address of this host (only for testing)
             host_addr = urllib.request.urlopen("https://api.zipixx.com/forwardedfor").read().decode("utf-8")
 
-        self.host_addr = (host_addr,self.port)
+        self.host_addr = (host_addr,self.port)  
+        
+        
+########################################### continue here with commenting #########################################
         
     def send_synchronize_request(self):
         for conn in self.connections:
